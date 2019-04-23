2ce4918f9dd3f4de8c8ecfb0ace0dbbbb5666f9c
Jakub Ziarko <jakub.ziarko@solteq.com>
2019-04-23 22:43:44 +0200

    correlation id enricher

---
+++ Serilog.Enrichers.CorrelationId/CorrelationIdEnricher.cs
@@ -0,0 +1,13 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace Serilog.Enrichers.CorrelationId
+{
+    public class CorrelationIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            propertyFactory.CreateProperty("CorrelationId", null);
+        }
+    }
+}
---
+++ Serilog.Enrichers.CorrelationId/CorrelationIdLoggerConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Serilog.Configuration;
+
+namespace Serilog.Enrichers.CorrelationId
+{
+    public static class CorrelationIdLoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration WithCorrelationId(
+            this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration)
+        {
+            if (loggerEnrichmentConfiguration == null) throw new ArgumentNullException(nameof(loggerEnrichmentConfiguration));
+            return loggerEnrichmentConfiguration.With<CorrelationIdEnricher>();
+        }
+
+    }
+}

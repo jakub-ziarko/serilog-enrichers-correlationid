fab16deedb132e6c7dfc58b0a7573f8c4d3a419e
Jakub Ziarko <jakub.ziarko@solteq.com>
2019-04-27 10:17:23 +0200

    dirty commit

--- Serilog.Enrichers.CorrelationId/CorrelationIdEnricher.cs
+++ Serilog.Enrichers.CorrelationId/CorrelationIdEnricher.cs
@@ -1,4 +1,6 @@
-﻿using Serilog.Core;
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Core;
 using Serilog.Events;
 
 namespace Serilog.Enrichers.CorrelationId
@@ -8,6 +10,26 @@ namespace Serilog.Enrichers.CorrelationId
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
             propertyFactory.CreateProperty("CorrelationId", null);
+        }
+    }
+
+
+    public static class CorrelationIdLoggerConfigurationExtensions
+    {
+        public static LoggerConfiguration WithCorrelationId(
+            this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration)
+        {
+            if (loggerEnrichmentConfiguration == null) throw new ArgumentNullException(nameof(loggerEnrichmentConfiguration));
+            return loggerEnrichmentConfiguration.With<CorrelationIdEnricher>();
+        }
+
+    }
+
+    public class CorrelationIdEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            propertyFactory.CreateProperty("CorrelationId", "asdadadasdafjasfljashfasljhfsa");
         }
     }
 }
